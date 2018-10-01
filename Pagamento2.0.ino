7566c70c4b4941dc843d21e2d57893a5a5789d0a
pedroaspaiva <43342577+pedroaspaiva@users.noreply.github.com>
2018-10-01 17:51:20 -0300

    Add files via upload

---
+++ Pagamento2.0.ino
@@ -0,0 +1,90 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 10;     // Configurable, see typical pin layout above
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522::MIFARE_Key key;
+/**
+ * Initialize.
+ */
+void setup(){
+    Serial.begin(9600); // Initialize serial communications with the PC
+    while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+    SPI.begin();        // Init SPI bus
+    mfrc522.PCD_Init(); // Init MFRC522 card
+
+        // Prepare the key (used both as key A and as key B)
+    // using FFFFFFFFFFFFh which is the default at chip delivery from the factory
+    for (byte i = 0; i < 6; i++) {
+        key.keyByte[i] = 0xFF;
+    }
+  
+}
+void loop(){
+  
+  
+    // Look for new cards
+    if ( ! mfrc522.PICC_IsNewCardPresent())
+        return;
+
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial())
+        return;
+    byte valor[]={0x00,0x00,0x00,0x00,
+                  0x00,0x00,0x00,0x00,
+                  0x00,0x00,0x00,0x00,
+                  0x00,0x00,0x00,0x00};
+    byte blockaddr = 4;
+   
+        
+
+  MFRC522::StatusCode status;
+    // Authenticate using key A
+    Serial.println(F("Authenticating using key A..."));
+    status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 7, &key, &(mfrc522.uid));
+       if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("PCD_Authenticate() failed: "));
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+    }
+             
+              // Show the whole sector as it currently is
+    Serial.println(F("Current data in sector:"));
+    mfrc522.PICC_DumpMifareClassicSectorToSerial(&(mfrc522.uid), &key, 1);
+    Serial.println();
+    
+  // Authenticate using key B
+    Serial.println(F("Authenticating again using key B..."));
+    status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, 7, &key, &(mfrc522.uid));
+           if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("PCD_Authenticate() failed: "));
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+    }
+    Serial.println("AA");
+
+  Serial.println("Digite um valor para recarregar");
+  do{
+    valor[0] = Serial.parseInt();
+    
+    }while(valor[0]<=0);
+    Serial.print(F("Seu valor é:")); Serial.println(valor[0]);
+
+    // Write data to the block
+   status = (MFRC522::StatusCode) mfrc522.MIFARE_Write(blockaddr, valor, 16);
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("MIFARE_Write() failed: "));
+        Serial.println(mfrc522.GetStatusCodeName(status));
+    }
+    Serial.println();
+Serial.println("Cabo");
+     // Halt PICC
+    mfrc522.PICC_HaltA();
+    // Stop encryption on PCD
+    mfrc522.PCD_StopCrypto1();
+    
+
+
+    
+  }
